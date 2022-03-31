80bbfdedaa9059a1bc5cd129b09ed57ca3bad123
dannymagnus <mrdanrichards@gmail.com>
2022-03-31 17:30:23 +0100

    add home app

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+def index(request):
+    """
+    A view to return the index page
+    """
+    return render(request, 'home/index.html')
