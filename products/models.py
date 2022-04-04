4731fe86ef488477b35de9520cf88beb9380547c
dannymagnus <mrdanrichards@gmail.com>
2022-04-04 15:44:42 +0100

    configure product admin

--- products/models.py
+++ products/models.py
@@ -4,6 +4,10 @@ class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254)
     
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
     def __str__(self):
         return self.name
     
